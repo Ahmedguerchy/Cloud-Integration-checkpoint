8ce7ff0c52e43b01544e7bd5f853c6fa8bd57d92
Ahmedguerchy <79759942+Ahmedguerchy@users.noreply.github.com>
2022-12-08 19:31:58 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Bizert%20zarzouna&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
+  );
+}
+
+export default App;
